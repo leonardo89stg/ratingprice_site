e4a0e79d04c6387a7c3eb641dbefd33378954d5a
Leonardo SilvaTorquato Gomes <leo89stg@gmail.com>
2025-06-05 02:01:01 -0300

    site novo  adm

--- ratingpricesite/urls.py
+++ ratingpricesite/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/admin_site", views.admin_site, name="admin_site"),
+    path("admin_site/", views.admin_site, name="admin_site"),
+    path("nw_user/", views.new_user, name="new_user"),
 ]
--- ratingpricesite/views.py
+++ ratingpricesite/views.py
@@ -12,27 +12,30 @@ def index(request):
 
 
 def admin_site(request):
+    return render(request,'admin.html')
    
-    context = {}
+"""   context = {}
 
-    msg = '''
-    <h2>Olá, este é um e-mail de teste</h2>
-    <p>Enviado automaticamente ao acessar a página.</p>
-    <button style="padding: 10px; background-color: #4CAF50; color: white;">Clique aqui</button>
-    '''
+msg = '''
+<h2>Olá, este é um e-mail de teste</h2>
+<p>Enviado automaticamente ao acessar a página.</p>
+<button style="padding: 10px; background-color: #4CAF50; color: white;">Clique aqui</button>
+'''
 
-    try:
-        send_mail(
-            'Teste automático',
-            '',  # corpo texto simples vazio
-            settings.EMAIL_HOST_USER,
-            ['leo89stg@gmail.com'],
-            html_message=msg,
-            fail_silently=False
-        )
-        context['result'] = 'Email enviado automaticamente.'
-    except Exception as e:
-        context['result'] = f'Erro ao enviar email: {e}'
+try:
+    send_mail(
+        'Teste automático',
+        '',  # corpo texto simples vazio
+        settings.EMAIL_HOST_USER,
+        ['leo89stg@gmail.com'],
+        html_message=msg,
+        fail_silently=False
+    )
+    context['result'] = 'Email enviado automaticamente.'
+except Exception as e:
+    context['result'] = f'Erro ao enviar email: {e}'
 
-    return render(request, "index.html", context)
+return render(request, "index.html", context) """
 
+def new_user(request):
+    return render(request,'account_new.html')
